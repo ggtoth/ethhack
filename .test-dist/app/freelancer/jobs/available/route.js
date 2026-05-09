29c9dcac577164541ed0e972b0a1549bfaf98ecc
martonalpha <martonbiro167@gmail.com>
2026-05-09 16:43:05 +0200

    Remove generated test output

--- .test-dist/app/freelancer/jobs/available/route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GET = GET;
-const dummy_endpoints_1 = require("@/lib/workflow/dummy-endpoints");
-async function GET() {
-    return Response.json((0, dummy_endpoints_1.listAvailableDummyJobs)().map(dummy_endpoints_1.toJobListItem));
-}
